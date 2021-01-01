6b1c945d61a20ab2d4c690e644abf0401cc006b4
Patrick Mukherjee <patrick.mukherjee@aequitec.ch>
2021-01-01 09:40:04 +0100

    extends ParseError

--- src/parse_error.rs
+++ src/parse_error.rs
@@ -3,24 +3,26 @@ use std::num::ParseIntError;
 
 #[derive(Debug)]
 pub enum ParseError {
-  NoneError,
-  ParseIntError(ParseIntError),
+    ValidationError,
+    NoneError,
+    ParseIntError(ParseIntError),
 }
 impl std::error::Error for ParseError {}
 impl fmt::Display for ParseError {
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    match self {
-      ParseError::NoneError => write!(f, "Input Sting is ill formated"),
-      ParseError::ParseIntError(err) => write!(
-        f,
-        "Input Sting is ill formated - expected an integer {}",
-        err
-      ),
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ParseError::ValidationError => write!(f, "Input Sting is not valid"),
+            ParseError::NoneError => write!(f, "Input Sting is ill formatted"),
+            ParseError::ParseIntError(err) => write!(
+                f,
+                "Input Sting is ill formatted - expected an integer {}",
+                err
+            ),
+        }
     }
-  }
 }
 impl std::convert::From<std::num::ParseIntError> for ParseError {
-  fn from(err: std::num::ParseIntError) -> Self {
-    ParseError::ParseIntError(err)
-  }
+    fn from(err: std::num::ParseIntError) -> Self {
+        ParseError::ParseIntError(err)
+    }
 }
