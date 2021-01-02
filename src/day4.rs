d1d69442b03f0492e0dae34f3f29f5d2f62cd403
Patrick Mukherjee <patrick.mukherjee@aequitec.ch>
2021-01-02 03:19:56 +0100

    part 2 test ok, but result wrong

--- src/day4.rs
+++ src/day4.rs
@@ -395,7 +395,7 @@ fn part2(passports: &[Passport]) -> usize {
 mod tests {
     use super::*;
 
-    fn sample_input() -> &'static str {
+    fn sample_input_part1() -> &'static str {
         "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
     byr:1937 iyr:2017 cid:147 hgt:1
     
@@ -416,13 +416,59 @@ mod tests {
     hcl:#cfa07d eyr:2025 pid:166559648
     iyr:2011 ecl:brn hgt:59in"
     }
-    fn parse_input_test() -> Vec<Passport> {
-        parse_input(sample_input())
+    fn parse_input_test_part1() -> Vec<Passport> {
+        parse_input(sample_input_part1())
     }
     #[test]
     fn test_part1() {
-        assert_eq!(4, part1(&parse_input_test()))
+        assert_eq!(4, part1(&parse_input_test_part1()))
     }
+
+    fn sample_input_part2_not_valid() -> &'static str {
+        "eyr:1972 cid:100
+      hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+      
+      iyr:2019
+      hcl:#602927 eyr:1967 hgt:170cm
+      ecl:grn pid:012533040 byr:1946
+      
+      hcl:dab227 iyr:2012
+      ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+      
+      hgt:59cm ecl:zzz
+      eyr:2038 hcl:74454a iyr:2023
+      pid:3556412378 byr:2007"
+    }
+    fn sample_input_part2_valid() -> &'static str {
+        "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+    hcl:#623a2f
+    
+    eyr:2029 ecl:blu cid:129 byr:1989
+    iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+    
+    hcl:#888785
+    hgt:164cm byr:2001 iyr:2015 cid:88
+    pid:545766238 ecl:hzl
+    eyr:2022
+    
+    iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719"
+    }
+
+    fn parse_input_test_part2_not_valid() -> Vec<Passport> {
+        parse_input(sample_input_part2_not_valid())
+    }
+    fn parse_input_test_part2_valid() -> Vec<Passport> {
+        parse_input(sample_input_part2_valid())
+    }
+    #[test]
+    fn test_part2_not_valid_input() {
+        assert_eq!(0, part2(&parse_input_test_part2_not_valid()))
+    }
+    #[test]
+    fn test_part2_valid_input() {
+        assert_eq!(4, part2(&parse_input_test_part2_valid()))
+    }
+
     // #[test]
     // fn test_ride_part2() {
     //   assert_eq!(336, part2(&parse_input()))
