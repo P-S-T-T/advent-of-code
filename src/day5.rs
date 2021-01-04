e5319163390bdc93b98c5982e61098cb1601da15
Patrick Mukherjee <patrick.mukherjee@aequitec.ch>
2021-01-04 09:36:46 +0100

    solves part2

--- src/day5.rs
+++ src/day5.rs
@@ -1,5 +1,3 @@
-use std::{cmp, fmt::Binary};
-
 /*
 --- Day 5: Binary Boarding ---
 
@@ -43,24 +41,39 @@ Here are some other boarding passes:
 
 As a sanity check, look through your list of boarding passes. What is the highest seat ID on a boarding pass?
 
+Your puzzle answer was 938.
+
+The first half of this puzzle is complete! It provides one gold star: *
+--- Part Two ---
+
+Ding! The "fasten seat belt" signs have turned on. Time to find your seat.
+
+It's a completely full flight, so your seat should be the only missing boarding pass in your list. However, there's a catch: some of the seats at the very front and back of the plane don't exist on this aircraft, so they'll be missing from your list as well.
+
+Your seat wasn't at the very front or back, though; the seats with IDs +1 and -1 from yours will be in your list.
+
+What is the ID of your seat?
+696
+That's the right answer! You are one gold star closer to saving your vacation.
 */
 // use crate::parse_error::ParseError;
+use std::cmp;
 
 #[aoc_generator(day5)]
 fn parse_input(input: &str) -> Vec<String> {
     input.lines().map(|str| str.trim().to_string()).collect()
 }
 
-fn decode_boarding_pass(boarding_code: &str) -> u16 {
-    let (row_code, column_code) = boarding_code.split_at(5);
+fn decode_boarding_pass(boarding_code: &str) -> u32 {
+    let (row_code, column_code) = boarding_code.split_at(7);
 
     let row = bin_convert_direct(row_code); //decode_code_sequence(row_code, 0, 127);
     let column = bin_convert_direct(column_code); // decode_code_sequence(column_code, 0, 7);
 
-    row as u16 * 8 + column as u16
+    row as u32 * 8 + column as u32
 }
 
-fn decode_code_sequence(code_sequence: &str, mut lowest: u8, mut highest: u8) -> u8 {
+fn decode_code_sequence(code_sequence: &str, lowest: u8, highest: u8) -> u8 {
     let (lowest, highest) = code_sequence
         .chars()
         .fold((lowest, highest), |(lowest, highest), code| {
@@ -97,15 +110,38 @@ fn bin_convert_direct(code_sequence: &str) -> u8 {
 }
 
 #[aoc(day5, part1)]
-fn part1(boarding_passes: &[String]) -> u16 {
+fn part1(boarding_passes: &[String]) -> u32 {
     boarding_passes.iter().fold(0, |highest, boarding_code| {
         cmp::max(highest, decode_boarding_pass(boarding_code))
     })
 }
 
-// #[aoc(day5, part2)]
-// fn part2(passports: &[Passport]) -> usize {
-// }
+#[aoc(day5, part2)]
+fn part2(boarding_passes: &[String]) -> u32 {
+    let mut seat_numbers = boarding_passes
+        .iter()
+        .map(|boarding_code| decode_boarding_pass(boarding_code))
+        .collect::<Vec<u32>>();
+
+    seat_numbers.sort_unstable();
+
+    let mut seats_iter = seat_numbers.iter();
+    let mut previous = seats_iter.next().unwrap();
+
+    let mut found_seat: u32 = 0;
+
+    for seat in seats_iter {
+        match seat - previous {
+            2 => {
+                found_seat = seat - 1;
+                break;
+            }
+            _ => previous = seat,
+        }
+    }
+
+    found_seat
+}
 
 #[cfg(test)]
 mod tests {
@@ -115,10 +151,10 @@ mod tests {
         "FBFBBFFRLR
          BFFFBBFRRR
          FFFBBBFRRR
-         BBFFBBFRLL
-        "
+         BBFFBBFLRL
+         BBFFBBFRLL"
     }
-    fn parse_test_input() -> Vec<&'static str> {
+    fn parse_test_input() -> Vec<String> {
         parse_input(sample_input_part1())
     }
     #[test]
@@ -129,8 +165,15 @@ mod tests {
         assert_eq!(567, decode_boarding_pass(boarding_code));
         let boarding_code = "FFFBBBFRRR";
         assert_eq!(119, decode_boarding_pass(boarding_code));
+        let boarding_code = "BBFFBBFLRL";
+        assert_eq!(818, decode_boarding_pass(boarding_code));
         let boarding_code = "BBFFBBFRLL";
         assert_eq!(820, decode_boarding_pass(boarding_code));
+    }
+    #[test]
+    fn test_max_seat_id() {
+        let boarding_code = "BBBBBBBRRR";
+        assert_eq!(1023, decode_boarding_pass(boarding_code));
     }
 
     #[test]
@@ -138,14 +181,14 @@ mod tests {
         assert_eq!(5, bin_convert_direct("RLR"));
         assert_eq!(44, bin_convert_direct("FBFBBFF"));
     }
-    // #[test]
-    // fn test_part1() {
 
-    //   assert_eq!(4, part1(&parse_input_test_part1()))
-    // }
+    #[test]
+    fn test_part1() {
+        assert_eq!(820, part1(&parse_test_input()))
+    }
 
-    // #[test]
-    // fn test_part2_valid_input() {
-    //     assert_eq!(4, part2(&parse_input_test_part2_valid()))
-    // }
+    #[test]
+    fn test_part2() {
+        assert_eq!(819, part2(&parse_test_input()))
+    }
 }
