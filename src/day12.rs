b0bb22bcdba48d29a295fbcc625cd9bc87fc61ac
Patrick Mukherjee <patrick@mukherjee.de>
2022-12-10 08:11:58 +0100

    solved part 1

--- src/day12.rs
+++ src/day12.rs
@@ -34,6 +34,36 @@ At the end of these instructions, the ship's Manhattan distance (sum of the abso
 starting position is 17 + 8 = 25.
 
 Figure out where the navigation instructions lead. What is the Manhattan distance between that location and the ship's starting position?
+
+Your puzzle answer was 1010.
+
+The first half of this puzzle is complete! It provides one gold star: *
+
+--- Part Two ---
+
+Before you can give the destination to the captain, you realize that the actual action meanings were printed on the back of the instructions the whole time.
+
+Almost all of the actions indicate how to move a waypoint which is relative to the ship's position:
+
+Action N means to move the waypoint north by the given value.
+Action S means to move the waypoint south by the given value.
+Action E means to move the waypoint east by the given value.
+Action W means to move the waypoint west by the given value.
+Action L means to rotate the waypoint around the ship left (counter-clockwise) the given number of degrees.
+Action R means to rotate the waypoint around the ship right (clockwise) the given number of degrees.
+Action F means to move forward to the waypoint a number of times equal to the given value.
+The waypoint starts 10 units east and 1 unit north relative to the ship. The waypoint is relative to the ship; that is, if the ship moves, the waypoint moves with it.
+
+For example, using the same instructions as above:
+
+F10 moves the ship to the waypoint 10 times (a total of 100 units east and 10 units north), leaving the ship at east 100, north 10. The waypoint stays 10 units east and 1 unit north of the ship.
+N3 moves the waypoint 3 units north to 10 units east and 4 units north of the ship. The ship remains at east 100, north 10.
+F7 moves the ship to the waypoint 7 times (a total of 70 units east and 28 units north), leaving the ship at east 170, north 38. The waypoint stays 10 units east and 4 units north of the ship.
+R90 rotates the waypoint around the ship clockwise 90 degrees, moving it to 4 units east and 10 units south of the ship. The ship remains at east 170, north 38.
+F11 moves the ship to the waypoint 11 times (a total of 44 units east and 110 units south), leaving the ship at east 214, south 72. The waypoint stays 4 units east and 10 units south of the ship.
+After these operations, the ship's Manhattan distance from its starting position is 214 + 72 = 286.
+
+Figure out where the navigation instructions actually lead. What is the Manhattan distance between that location and the ship's starting position?
 */
 
 #[derive(Clone)]
@@ -61,10 +91,10 @@ impl Direction {
         let direction = (*self as isize + turns) % 4;
 
         match direction {
-            0 => Self::West,
-            1 => Self::South,
-            2 => Self::East,
-            3 => Self::North,
+            0 => Self::East,
+            -3 | 1 => Self::South,
+            -2 | 2 => Self::West,
+            -1 | 3 => Self::North,
             _ => panic!("impossible!"),
         }
     }
@@ -167,6 +197,42 @@ F11
     #[test]
     fn test_part1_samples() {
         assert_eq!(25, part1(&parse_input(SAMPLE_INPUT_1)));
+    }
+    const SAMPLE_INPUT_2: &str = "F10
+R270
+F10
+";
+
+    #[test]
+    fn test_part1_sample_2() {
+        assert_eq!(20, part1(&parse_input(SAMPLE_INPUT_2)));
+    }
+    const SAMPLE_INPUT_3: &str = "F10
+R540
+F10
+";
+
+    #[test]
+    fn test_part1_sample_3() {
+        assert_eq!(0, part1(&parse_input(SAMPLE_INPUT_3)));
+    }
+    const SAMPLE_INPUT_4: &str = "F10
+L270
+F10
+";
+
+    #[test]
+    fn test_part1_sample_4() {
+        assert_eq!(20, part1(&parse_input(SAMPLE_INPUT_4)));
+    }
+    const SAMPLE_INPUT_5: &str = "F10
+R540
+F10
+";
+
+    #[test]
+    fn test_part1_sample_5() {
+        assert_eq!(0, part1(&parse_input(SAMPLE_INPUT_5)));
     }
 
     // #[test]
